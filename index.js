974ce03ec943c9ad75b1f89fbf9a433d5d200899
FoysalAhammed <hanif.mia2233@gmail.com>
2024-05-13 09:29:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("toggleMenuBtn").addEventListener("click", function() {
+    var menu = document.getElementById("navMenu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  });
+  
